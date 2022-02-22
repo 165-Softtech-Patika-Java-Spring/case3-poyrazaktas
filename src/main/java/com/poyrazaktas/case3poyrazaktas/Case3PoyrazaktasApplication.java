9aeb0b47a2b2405081e9f2a037540b981719732b
Poyraz Aktas <poyraz.educational@gmail.com>
2022-02-22 22:11:16 +0300

    init spring boot project

---
+++ src/main/java/com/poyrazaktas/case3poyrazaktas/Case3PoyrazaktasApplication.java
@@ -0,0 +1,13 @@
+package com.poyrazaktas.case3poyrazaktas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case3PoyrazaktasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Case3PoyrazaktasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poyrazaktas/case3poyrazaktas/Case3PoyrazaktasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poyrazaktas.case3poyrazaktas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Case3PoyrazaktasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
