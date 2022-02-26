cf8b248345ffc9dcf1a86f42ac64987768d641a1
Poyraz Aktas <poyraz.educational@gmail.com>
2022-02-26 13:45:32 +0300

    change query annotation to data derived queries

--- src/main/java/com/poyrazaktas/case3poyrazaktas/rev/dao/RevReviewDao.java
+++ src/main/java/com/poyrazaktas/case3poyrazaktas/rev/dao/RevReviewDao.java
@@ -2,16 +2,13 @@ package com.poyrazaktas.case3poyrazaktas.rev.dao;
 
 import com.poyrazaktas.case3poyrazaktas.rev.entity.RevReview;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface RevReviewDao extends JpaRepository<RevReview, Long> {
-    @Query("SELECT review from RevReview review where review.userId=?1")
     List<RevReview> findAllByUserId(Long userId);
 
-    @Query("SELECT review from RevReview review where review.productId=?1")
     List<RevReview> findAllByProductId(Long productId);
 }
